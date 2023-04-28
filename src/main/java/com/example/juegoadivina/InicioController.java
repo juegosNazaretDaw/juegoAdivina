5018bb5dad36fd2856a417151e86db100c341117
Iker Soto <ik.soto@aulanz.net>
2023-04-28 10:09:18 +0200

    añadir instrucciones pagina y editar inicio, boton instrucciones

--- src/main/java/com/example/juegoadivina/InicioController.java
+++ src/main/java/com/example/juegoadivina/InicioController.java
@@ -20,6 +20,13 @@ public class InicioController {
         stage.setScene(scene);
         stage.show();
     }
+    public void VerInstrucciones(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("InstruccionesView.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 
 
