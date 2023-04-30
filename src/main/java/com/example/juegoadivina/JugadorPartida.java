622b560b7d3f0aa4bdfe24c3cbd129cd9f35b574
issam-nz <is.elouafa@aulanz.net>
2023-04-30 20:36:29 +0200

    modificar Jugador - added JugadorPartida and Encryption for password

---
+++ src/main/java/com/example/juegoadivina/JugadorPartida.java
@@ -0,0 +1,23 @@
+package com.example.juegoadivina;
+
+public class JugadorPartida extends Jugador{
+    //se puede no ser hija de Jugador
+    //tendra un id, vidas y vivo(true o false)
+    public static int id = 0;
+    public int vidas;
+    public boolean vivo;
+
+    JugadorPartida(String nombre, String password) {
+        super(nombre, password);
+        id++;
+        vidas = 5;
+        vivo = true;
+    }
+
+    public JugadorPartida(int ranking, String nombre, String password, int partidasJugadas, int partidasGanadas, int puntos) {
+        super(ranking, nombre, password, partidasJugadas, partidasGanadas, puntos);
+        id++;
+        vidas = 5;
+        vivo = true;
+    }
+}
