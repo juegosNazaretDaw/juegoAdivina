fc81f2f398ff8ee644d709526d7b9949da9d735a
Iker Soto <ik.soto@aulanz.net>
2023-05-02 11:47:50 +0200

    Ranking General View y Controller añadido

--- src/main/java/com/example/juegoadivina/InicioController.java
+++ src/main/java/com/example/juegoadivina/InicioController.java
@@ -28,6 +28,14 @@ public class InicioController {
         stage.show();
     }
 
+    public void VerRanking(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("RankingGeneralView.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 
 }
