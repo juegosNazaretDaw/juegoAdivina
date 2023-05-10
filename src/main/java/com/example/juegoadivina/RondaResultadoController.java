897bfcbdb0adeee50f3d74ceb80bbaaf4f20ef4d
issam <is.elouafa@aulanz.net>
2023-05-10 13:41:51 +0200

    seguir con rondaResultado

--- src/main/java/com/example/juegoadivina/RondaResultadoController.java
+++ src/main/java/com/example/juegoadivina/RondaResultadoController.java
@@ -1,10 +1,24 @@
 package com.example.juegoadivina;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class RondaResultadoController {
 
 
+    @FXML
+    private TableView<Jugador> jugadorTableView;
+    @FXML
+    private TableColumn<Jugador, String> jugadorColumn;
+    @FXML
+    private TableColumn<Jugador, Integer> vidasColumn;
+    @FXML
+    private TableColumn<Jugador, Integer> numeroElejidoColumn;
 
-
+    private ObservableList<JugadorPartida> jugadoresRonda = FXCollections.observableArrayList();
 
 
 }
